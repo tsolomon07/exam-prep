cb445211b28d024d9e37ff983ba77f175b4b950a
Tamryn Solomons <tsolomon@c4r12s6.wethinkcode.co.za>
2020-01-28 15:49:35 +0200

    exam prep aff a

--- aff_a.c
+++ aff_a.c
@@ -6,7 +6,7 @@
 /*   By: tsolomon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 15:02:10 by tsolomon          #+#    #+#             */
-/*   Updated: 2020/01/27 15:10:01 by tsolomon         ###   ########.fr       */
+/*   Updated: 2020/01/28 15:46:55 by tsolomon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,21 @@
 
 int			main(int argc, char **argv)
 {
-	int i;
-
+	int i;	
 	i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] != '\0')
+		while  (argv[1][i] != '\0' && argv[1][i]!='a')
 		{
-			if (argv[1][i] == 'a') //if current(i) character is equal to the chracter 'a'
-			{
-				write(1, "a", 1); //display 'a' if the above condition is met
-				break ; //to exit the while loop after after displaying 'a'
-			}
-			i++;
+		i++;
+		}
+
+		if (argv[1][i]=='a')
+		{
+			write(1,"a",1);
 		}
 	}
-	else if (argc == 1)
-	{
-		write(1, "a", 1);
-	}
+
 	write(1, "\n", 1);
 	return (0);
 }
